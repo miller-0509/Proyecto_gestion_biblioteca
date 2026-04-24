0df1227a052f194f8a4d3bc0b08e00ebee059f2c
miller-0509 <caperamiller5@gmail.com>
2026-04-24 16:55:42 -0500

    Update run.py

--- run.py
+++ run.py
@@ -2,15 +2,13 @@ from app import create_app, db
 
 app = create_app()
 
-# Inicializar la base de datos dentro del contexto de la aplicación
 with app.app_context():
     db.create_all()
     print("✓ Base de datos inicializada correctamente.")
 
-# Manejador para asegurar que los cambios se guarden antes de cerrar
 @app.teardown_appcontext
 def shutdown_session(exception=None):
-    """Cierra la sesión de la base de datos de forma correcta"""
+
     if exception is None:
         db.session.commit()
     db.session.remove()
