0d06d28c6f2a3f8b4714cd870dd0d1d263bfa5af
miller-0509 <caperamiller5@gmail.com>
2026-05-07 19:01:34 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,10 @@ from flask_wtf.csrf import CSRFProtect
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from werkzeug.exceptions import HTTPException
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 login_manager = LoginManager()
 csrf = CSRFProtect()
 
