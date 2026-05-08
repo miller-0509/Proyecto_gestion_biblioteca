7693467a496dad051b1a3d6f47932341a7ac7658
miller-0509 <caperamiller5@gmail.com>
2026-05-07 19:50:27 -0500

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,16 @@ class Config:
         'dev-cambiar-en-produccion-obligatorio'
     )
 
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    database_url = os.getenv("DATABASE_URL")
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
+SQLALCHEMY_DATABASE_URI = database_url
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
